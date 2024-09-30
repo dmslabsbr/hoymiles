ed883e0a2444f360ed986e36c243dc6feae1dc36
cosik <sentyrz.m@gmail.com>
2024-09-30 09:04:30 +0200

    updated payloads

--- edge/src/hoymiles/cloud_payloads.py
+++ edge/src/hoymiles/cloud_payloads.py
@@ -3,11 +3,11 @@ File contain dataclasses used in addon. Each data class represent part of payloa
 
 Here is example how to use it:
 
-    from hoymiles.cloud_payloads import TokenBody, TokenPayload
+    from hoymiles.cloud_payloads import TokenBody, Payload
     from dataclasses import asdict
 
     body = TokenBody("asxd", "asxd")
-    payload = TokenPayload(body)
+    payload = Payload(body)
     print(asdict(payload))
 """
 
@@ -26,31 +26,31 @@ class LoadBody:
 
 
 @dataclass(repr=True)
-class TokenPayload:
-    body: TokenBody
+class Payload:
+    body: Body
     ERROR_BACK: bool = True
     LOAD: LoadBody = LoadBody()
     WAITING_PROMISE: bool = True
 
 
 @dataclass(repr=True)
-class TokenBody:
+class TokenBody(Body):
     password: str
     user_name: str
 
 
 @dataclass(repr=True)
-class SidBody:
+class SidBody(Body):
     sid: str
 
 
 @dataclass(repr=True)
-class IdBody:
+class IdBody(Body):
     id: str
 
 
 @dataclass(repr=True)
-class InverterBody:
+class InverterBody(Body):
     mi_id: str
     mi_sn: str
     time: str
